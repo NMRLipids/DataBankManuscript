6c04d06961510db87a89d2ea0e2885a22fc2b505
Ollila Samuli O H <osollila@dx0-919-22499.ad.helsinki.fi>
2022-03-24 08:35:31 +0200

    Add water diffusion results

---
+++ scripts/calcMD-PERMEATION.py
@@ -0,0 +1,169 @@
+import os
+import sys
+import numpy as np
+
+import yaml
+import json
+import matplotlib.pyplot as plt
+import mdtraj
+import urllib.request
+import seaborn as sns
+
+import MDAnalysis as mda
+#from MDAnalysis.analysis.waterdynamics import MeanSquareDisplacement as MSD
+
+sys.path.insert(1, '../../Databank/Scripts/BuildDatabank/')
+from databankLibrary import download_link, read_mapping_file, read_mapping_file_res, read_mapping_filePAIR, make_positive_angles, lipids_dict, molecules_dict, databank
+
+path = '../../Databank/Data/Simulations/'
+db_data = databank(path)
+systems = db_data.get_systems()
+
+molecules = []
+for key in lipids_dict:
+    molecules.append(key)
+for key in molecules_dict:
+    molecules.append(key)
+
+
+def getLipids(readme, molecules=lipids_dict.keys()):
+    lipids = 'resname '
+
+    for key in readme['COMPOSITION'].keys():
+        if key in molecules:
+            m_file = readme['COMPOSITION'][key]['MAPPING']
+            with open('../../Databank/Scripts/BuildDatabank/mapping_files/'+m_file,"r") as f:
+                for line in f:
+                    if len(line.split()) > 2 and "Individual atoms" not in line:
+                        if line.split()[2] not in lipids:
+                            lipids = lipids + line.split()[2] + ' or resname '
+                    elif "Individual atoms" in line:
+                        continue
+                    else:
+                        lipids = lipids + readme['COMPOSITION'][key]['NAME'] + ' or resname '
+                        break    
+    lipids = lipids[:-12]
+    print(lipids)
+    return lipids
+
+
+
+cwd = os.getcwd()
+# Loop over simulations
+for system in systems:
+
+    # Extracting information from README.yaml file
+    subdir = system['path']
+    READMEfilepath = subdir + '/README.yaml'
+    doi = system['DOI']
+    trj = system.get('TRJ')
+    tpr = system.get('TPR')
+    trj_name = subdir + '/' + system.get('TRJ')[0][0]
+    tpr_name = subdir + '/' + system.get('TPR')[0][0]
+    gro_name = subdir + '/conf.gro'
+    trj_url = download_link(doi, trj[0][0])
+    tpr_url = download_link(doi, tpr[0][0])
+    EQtime=float(system.get('TIMELEFTOUT'))*1000
+
+    print(subdir)
+
+    # Create folders where water diffusion results will be saved
+    # This follows the organization of simulations in the original databank
+    outputFOLDERS = subdir.replace("../../Databank/Data/Simulations/","../Data/MD-PERMEATION/")    
+    #outfilename = str(densityFOLDERS) + '/WATERlateralMSD.xvg'
+
+    CheckOutPutFile = outputFOLDERS + '/Counting_events.txt'
+    if os.path.isfile(CheckOutPutFile): # and os.path.getsize(CheckOutPutFile) > 0:
+        print('Result already found')
+        continue
+    
+
+    
+    os.system('mkdir -p ' + outputFOLDERS)
+    os.system('cp ' + READMEfilepath + ' ' + outputFOLDERS)
+
+    #print(outfilename)                    
+    #if os.path.isfile(outfilename):
+    #    print('Result found from ' + subdir)
+    #    continue
+
+    print('Analyzing in ' + subdir)
+                        
+    # Downloading the tpr and xtc files locally using the information from the README.yaml file
+    if (not os.path.isfile(tpr_name)):
+        response = urllib.request.urlretrieve(tpr_url, tpr_name)
+                        
+    if (not os.path.isfile(trj_name)):
+        response = urllib.request.urlretrieve(trj_url, trj_name)
+
+    # Creating a gro file (this works only for Gromacs simulations)
+    if (not os.path.isfile(gro_name)):
+        os.system('echo System | gmx trjconv -f ' + trj_name + ' -s ' + tpr_name + ' -dump 0 -o ' + gro_name)
+
+    # Creating a trajectory with molecules whole  (this works only for Gromacs simulations) 
+    # Note that this leaves out the equilibration time
+    xtcwhole=subdir + '/whole.xtc'
+    if (not os.path.isfile(xtcwhole)):
+        os.system('echo System | gmx trjconv -f ' + trj_name + ' -s ' + tpr_name + ' -o ' + xtcwhole + ' -pbc mol -b ' + str(EQtime))
+                        
+    # Reads the name of water oxygen atom using the information in mapping file and README.yaml 
+    water_mapping_file = '../../Databank/Scripts/BuildDatabank/mapping_files/' + system['COMPOSITION']['SOL']['MAPPING']
+    waterO = read_mapping_file(water_mapping_file,'M_O_M')
+
+    # Reads the phosphate atom name
+    for lipid in system['COMPOSITION']:
+        if ('CHOL' in lipid) or ('CER' in lipid) or ('DHMDMAB' in lipid):
+            continue
+        elif lipid in lipids_dict:
+            lipid_mapping_file = '../../Databank/Scripts/BuildDatabank/mapping_files/' + system['COMPOSITION'][lipid]['MAPPING']
+            print(lipid_mapping_file)
+            P = read_mapping_file(lipid_mapping_file,'M_G3P2_M')
+
+    # Creating OW.gro and P.gro this works only for Gromacs simulations) 
+    ndxSTR = 'keep 0 \na ' + waterO  + ' \nkeep 1 \na' + P +  ' \nq'
+    if (not os.path.isfile(outputFOLDERS + '/OWPindex.ndx')):
+        os.system('echo "' + ndxSTR + '" | gmx make_ndx -f ' + tpr_name + ' -o ' + outputFOLDERS + '/OWPindex.ndx')
+    if (not os.path.isfile(outputFOLDERS + '/OW.gro')):
+        os.system('echo "' + waterO + '" | gmx trjconv -f ' + xtcwhole + ' -s ' + tpr_name + ' -n  ' + outputFOLDERS + '/OWPindex.ndx ' + ' -o ' + outputFOLDERS + '/OW.gro')
+    if (not os.path.isfile(outputFOLDERS + '/P.gro')):
+        os.system('echo "' + P + '" | gmx trjconv -f ' + xtcwhole + ' -s ' + tpr_name + ' -n  ' + outputFOLDERS + '/OWPindex.ndx ' + ' -o ' + outputFOLDERS + '/P.gro')
+
+    # Reading estimated center, simulation time, precision, and the number of the first water residue
+
+    # center of mass
+    u = mda.Universe(gro_name,xtcwhole)
+    lipids = getLipids(system)
+    c = u.select_atoms(lipids)
+    ctom = c.atoms.center_of_mass()[2]
+    ctom = ctom * 0.1  # change units to nm
+    
+    # simulation time (ns)
+    time = int(system['TRJLENGTH']*0.001 - system['TIMELEFTOUT'])
+    #print(time)
+
+    # precision
+    # print("'grep "t="' + outputFOLDERS + "/OW.gro  | head -n 2 | awk '{if(NR ==1 ) v1 = $8; if(NR == 2) v2 = $8}END{print v2-v1}' > "  + outputFOLDERS + 'precision.dat'")
+    #os.system('grep "t="' + outputFOLDERS + "/OW.gro  | head -n 2 | awk '{if(NR ==1 ) v1 = $8; if(NR == 2) v2 = $8}END{print v2-v1}' > "  + outputFOLDERS + 'precision.dat')
+    #with open(outputFOLDERS + 'precision.dat', "r") as presFILE:
+    #    precision = presFILE.read()
+    #print(precision)
+
+    precision = int(u.trajectory.dt)
+    print(precision)
+
+    
+    # first water residue
+    os.system("awk 'FIELDWIDTHS = '5' {if(NR == 3){ printf $1; exit}}' " +  outputFOLDERS + '/OW.gro > ' + outputFOLDERS + '/Wresidue.dat')
+    with open(outputFOLDERS + 'Wresidue.dat', "r") as resFILE:
+        Wresidue = resFILE.read()
+
+    os.system('cp ./MD-permeation ' + outputFOLDERS)
+    print('Changing to', outputFOLDERS)
+    os.chdir(outputFOLDERS)
+    print('./MD-permeation ' + str(ctom) + ' ' + str(time) +  ' ' + str(precision) + ' ' + str(Wresidue))
+    os.system('./MD-permeation ' + str(ctom) + ' ' + str(time) +  ' ' + str(precision) + ' ' + str(Wresidue))
+    print('Changing to', cwd)
+    os.chdir(cwd)
+
+    os.system("rm " +  outputFOLDERS + '/OW.gro')
+    
--- scripts/calcWATERdiffusion.py
+++ scripts/calcWATERdiffusion.py
@@ -18,127 +18,112 @@ import seaborn as sns
 import MDAnalysis
 from MDAnalysis.analysis.waterdynamics import MeanSquareDisplacement as MSD
 
-sys.path.insert(1, '../BuildDatabank/')
-from databankLibrary import download_link, read_mapping_file, read_mapping_file_res, read_mapping_filePAIR, make_positive_angles, lipids_dict, molecules_dict
 
-# Calculate density profiles of all molecules
+sys.path.insert(1, '../../Databank/Scripts/BuildDatabank/')
+from databankLibrary import download_link, read_mapping_file, read_mapping_file_res, read_mapping_filePAIR, make_positive_angles, lipids_dict, molecules_dict, databank
 
-#molecules = {'POPS','POPE','POPG','POPC','POT','SOD','CLA','CAL','SOL'}
+path = '../../Databank/Data/Simulations/'
+db_data = databank(path)
+systems = db_data.get_systems()
+
 molecules = []
 for key in lipids_dict:
     molecules.append(key)
 for key in molecules_dict:
     molecules.append(key)
-#lipids = {'POPS','POPE','POPG','POPC'}
-#lipids = {'POPE'}
-#atom1 = 'M_G3P2_M'
-#atom2 = 'M_G3C5O1_M'   #for POPG
-#atom2 = 'M_G3N6_M'
 
-colors = {'POPC' :'black','POPS':'red','POPE':'blue','POPG':'green'}
+    
+#colors = {'POPC' :'black','POPS':'red','POPE':'blue','POPG':'green'}
 
-h = []
+#h = []
 
 
-for subdir, dirs, files in os.walk(r'../../Data/Simulations/'):
-    for filename in files:
-        filepath = subdir + os.sep + filename
-        if filepath.endswith("README.yaml"):
-            READMEfilepath = subdir + '/README.yaml'
-            with open(READMEfilepath) as yaml_file:
-                readme = yaml.load(yaml_file, Loader=yaml.FullLoader)
-                #for molname in molecules:
-                doi = readme.get('DOI')
-                trj = readme.get('TRJ')
-                tpr = readme.get('TPR')
-                trj_name = subdir + '/' + readme.get('TRJ')[0][0]
-                tpr_name = subdir + '/' + readme.get('TPR')[0][0]
-                gro_name = subdir + '/conf.gro'
-                trj_url = download_link(doi, trj[0][0])
-                tpr_url = download_link(doi, tpr[0][0])
-                EQtime=float(readme.get('TIMELEFTOUT'))*1000
+#for subdir, dirs, files in os.walk(r'../../Data/Simulations/'):
+#    for filename in files:
+#        filepath = subdir + os.sep + filename
+#        if filepath.endswith("README.yaml"):
+#            READMEfilepath = subdir + '/README.yaml'
+#            with open(READMEfilepath) as yaml_file:
 
-                densityFOLDERS = subdir.replace("Simulations","WATERdiffusion")    
-                outfilename = str(densityFOLDERS) + '/WATERlateralMSD.xvg'
-                #indexfilename = str(densityFOLDERS) + '/' + molname + 'index.ndx' 
+for system in systems:
 
-                #    try:
-                #        nmol = int(readme['N' + molname])
-                #    except:
-                #        try:
-                #            nmol = int(readme['N' + molname][0])
-                #        except:
-                #            continue
+#                readme = yaml.load(yaml_file, Loader=yaml.FullLoader)
+#                #for molname in molecules:
+#                doi = readme.get('DOI')
+#                trj = readme.get('TRJ')
+#                tpr = readme.get('TPR')
+#                trj_name = subdir + '/' + readme.get('TRJ')[0][0]
+#                tpr_name = subdir + '/' + readme.get('TPR')[0][0]
+#                gro_name = subdir + '/conf.gro'
+#                trj_url = download_link(doi, trj[0][0])
+#                tpr_url = download_link(doi, tpr[0][0])
+#                EQtime=float(readme.get('TIMELEFTOUT'))*1000
 
-                print(outfilename)                    
-                if not os.path.isfile(outfilename):
-                                            
-                    print('Analyzing in '+filepath)
+#                densityFOLDERS = subdir.replace("Simulations","WATERdiffusion")    
+#                outfilename = str(densityFOLDERS) + '/WATERlateralMSD.xvg'
+
+
+    # Extracting information from README.yaml file
+    subdir = system['path']
+    READMEfilepath = subdir + '/README.yaml'
+    doi = system['DOI']
+    trj = system.get('TRJ')
+    tpr = system.get('TPR')
+    trj_name = subdir + '/' + system.get('TRJ')[0][0]
+    tpr_name = subdir + '/' + system.get('TPR')[0][0]
+    gro_name = subdir + '/conf.gro'
+    trj_url = download_link(doi, trj[0][0])
+    tpr_url = download_link(doi, tpr[0][0])
+    EQtime=float(system.get('TIMELEFTOUT'))*1000
+
+    print(subdir)
+
+    # Create folders where water diffusion results will be saved
+    # This follows the organization of simulations in the original databank
+    outputFOLDERS = subdir.replace("../../Databank/Data/Simulations/","../Data/WATERdiffusion/")    
+    outfilename = outputFOLDERS + '/WATERlateralMSD.xvg'
+    
+    #CheckOutPutFile = outputFOLDERS + '/WATERlateralMSD.xvg'
+    if os.path.isfile(outfilename): # and os.path.getsize(CheckOutPutFile) > 0:
+        print('Result already found')
+        continue
+
+    os.system('mkdir -p ' + outputFOLDERS)
+    os.system('cp ' + READMEfilepath + ' ' + outputFOLDERS)
+    
+    print('Analyzing in ' + subdir)
                         
-                    #Download tpr and xtc files to same directory where dictionary and data are located
-                    if (not os.path.isfile(tpr_name)):
-                        response = urllib.request.urlretrieve(tpr_url, tpr_name)
-                        
-                    if (not os.path.isfile(trj_name)):
-                        response = urllib.request.urlretrieve(trj_url, trj_name)
-                        
-                    fig= plt.figure(figsize=(12,9))
-                    if (not os.path.isfile(gro_name)):
-                        get_ipython().system('echo System | gmx trjconv -f {trj_name} -s {tpr_name}  -dump 0 -o {gro_name}')
-                        
-                    xtcwhole=subdir + '/whole.xtc'
-                    if (not os.path.isfile(xtcwhole)):
-                        get_ipython().system('echo System | gmx trjconv -f {trj_name} -s {tpr_name} -o {xtcwhole} -pbc mol -b {EQtime}')
-                        
-                        
-                    os.system('mkdir -p ' + densityFOLDERS)
-                    os.system('cp ' + READMEfilepath + ' ' + densityFOLDERS)
+    #Download tpr and xtc files to same directory where dictionary and data are located
 
-                    #u = MDAnalysis.Universe(gro_name, trj_name)
-                    water_mapping_file = '../BuildDatabank/mapping_files/'+readme['MAPPING_DICT']['SOL']
-                    waterO = read_mapping_file(water_mapping_file,'M_O_M')
-                    #waterRES = readme.get('SOL')
+
+    if (not os.path.isfile(tpr_name)):
+        response = urllib.request.urlretrieve(tpr_url, tpr_name)
+                        
+    if (not os.path.isfile(trj_name)):
+        response = urllib.request.urlretrieve(trj_url, trj_name)
+
+    # Creating a gro file (this works only for Gromacs simulations)
+    if (not os.path.isfile(gro_name)):
+        os.system('echo System | gmx trjconv -f ' + trj_name + ' -s ' + tpr_name + ' -dump 0 -o ' + gro_name)
+
+    # Creating a trajectory with molecules whole  (this works only for Gromacs simulations) 
+    # Note that this leaves out the equilibration time
+    xtcwhole=subdir + '/whole.xtc'
+    if (not os.path.isfile(xtcwhole)):
+        os.system('echo System | gmx trjconv -f ' + trj_name + ' -s ' + tpr_name + ' -o ' + xtcwhole + ' -pbc mol -b ' + str(EQtime))
+
+        
+        
+    #u = MDAnalysis.Universe(gro_name, trj_name)
+    #water_mapping_file = '../BuildDatabank/mapping_files/'+readme['MAPPING_DICT']['SOL']
+    water_mapping_file = '../../Databank/Scripts/BuildDatabank/mapping_files/' + system['COMPOSITION']['SOL']['MAPPING']
+    waterO = read_mapping_file(water_mapping_file,'M_O_M')
+    #waterRES = readme.get('SOL')
                     
-                    #os.system('echo System | gmx trjconv -f ' + xtc + ' -s ' + tpr + ' -o ' + xtcwhole + ' -pbc mol -b ' + str(EQtime))
-
-                    ndxSTR = 'keep 0 \na ' + waterO  + ' \nkeep 1 \nq'
-                    #print(ndxSTR)
-                    os.system('echo "' + ndxSTR + '" | gmx make_ndx -f ' + tpr_name + ' -o ' + densityFOLDERS + '/SOLindex.ndx') 
-                    os.system('gmx msd -f ' + xtcwhole + ' -s ' + tpr_name + ' -n  ' + densityFOLDERS + '/SOLindex.ndx -o ' + outfilename + ' -trestart 1000 -lateral z')
+    #os.system('echo System | gmx trjconv -f ' + xtc + ' -s ' + tpr + ' -o ' + xtcwhole + ' -pbc mol -b ' + str(EQtime))
+    
+    ndxSTR = 'keep 0 \na ' + waterO  + ' \nkeep 1 \nq'
+    #print(ndxSTR)
+    os.system('echo "' + ndxSTR + '" | gmx make_ndx -f ' + tpr_name + ' -o ' + outputFOLDERS + '/SOLindex.ndx') 
+    os.system('gmx msd -f ' + xtcwhole + ' -s ' + tpr_name + ' -n  ' + outputFOLDERS + '/SOLindex.ndx -o ' + outfilename + ' -trestart 1000 -lateral z')
                     
-                    #select = "byres name " + waterOX
-                    #MSD_analysis = MSD(u, select, 0, 1000, 20)
-                    #MSD_analysis.run()
-                
-                    #NcenterMOL = 0
-                    #for lipid in lipids_dict:
-                    #    if readme.get('N' + lipid):
-                    #        if int(readme.get('N' + lipid)[0]) > NcenterMOL:
-                    #            mapping_file = '../BuildDatabank/mapping_files/'+readme['MAPPING_DICT'][lipid] # readme.get('MAPPING')[0][0]
-                    #            try:
-                    #                centerMOL = read_mapping_file_res(mapping_file,'M_G3P2_M')
-                    #            except:
-                    #                centerMOL = readme.get(lipid)
-                    #            NcenterMOL = int(readme.get('N' + lipid)[0])
-
-                    
-                        #if (not os.path.isfile(outfilename)):
-                        #    try:
-                        #        mapping_file = '../BuildDatabank/mapping_files/'+readme['MAPPING_DICT'][molname] # readme.get('MAPPING')[0][0]
-                        #        resname = read_mapping_file_res(mapping_file,'M_G3P2_M')
-                        #    except:
-                        #        resname = readme.get(molname)
-                        #    try:
-                        #        get_ipython().system('echo "r {centerMOL} \n name 0 centerMOL \n r {resname} \n q" | gmx make_ndx -f {tpr_name} -o {indexfilename} -n /media/osollila/Data/NMRlipids/NMRlipidsIVPEandPG/scripts/init.ndx')
-                        #        get_ipython().system('echo "centerMOL \n  {resname} \n q" | gmx density -f {xtcwhole} -s {tpr_name} -o {outfilename} -b {EQtime} -n {indexfilename} -xvg none -center -dens number')
-                        #    except:
-                        #        print('Density calculation failed.')
-
-            
-
-
-
-
-
-
-
