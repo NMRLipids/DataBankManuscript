d16ef7638cc0c3d100828482d510b26d57fd2432
Ollila Samuli O H <osollila@dx0-919-22499.ad.helsinki.fi>
2022-07-01 18:56:52 +0300

    Update flip-flop data

--- scripts/FlipFlop.py
+++ scripts/FlipFlop.py
@@ -17,8 +17,8 @@ from urllib.error import URLError,HTTPError,ContentTooShortError
 
 import MDAnalysis as mda
 
-sys.path.insert(1, '../BuildDatabank/')
-from databankLibrary import download_link, lipids_dict
+sys.path.insert(1, '../../Databank/Scripts/BuildDatabank/')
+from databankLibrary import download_link, lipids_dict, databank
 
 def loadMappingFile(path_to_mapping_file):
     # load mapping file into a dictionary
@@ -33,13 +33,13 @@ def getLipids(readme):
         lipids = []
         for key in lipids_dict.keys():
             try:
-                if readme['N'+key] != [0,0]: 
+                if key in readme['COMPOSITION'].keys():                
+       #         if readme['N'+key] != [0,0]: 
                     lipids.append(key)
             except KeyError:
                 continue
         return lipids
         
-
 def getAtoms(readme, lipid):
     atoms = ""
     path_to_mapping_file = readme['COMPOSITION'][lipid]['MAPPING']
@@ -49,44 +49,51 @@ def getAtoms(readme, lipid):
   
     return atoms
 
-
 def membraneCentreOfMass(universe, readme):
     lipids = []
     for key_mol in lipids_dict:
-        selection = ""
-        if key_mol in readme['MAPPING_DICT'].keys():
-            m_file = readme['MAPPING_DICT'][key_mol]
-            with open('../BuildDatabank/mapping_files/'+m_file,"r") as f:
-                for line in f:
-                    if len(line.split()) > 2 and "Individual atoms" not in line:
-                        selection = selection + "(resname " + line.split()[2] + " and name " + line.split()[1] + ") or "
-                    elif "Individual atoms" in line:
-                       continue
-                    else:
-                        selection = "resname " + readme[key_mol]
+        if key_mol in readme['COMPOSITION'].keys():
+            selection = ""
+            m_file = readme['COMPOSITION'][key_mol]['MAPPING']
+            
+            with open('../../Databank/Scripts/BuildDatabank/mapping_files/'+m_file,"r") as f:
+                mapping = yaml.load(f, Loader=yaml.FullLoader)
+
+            for atom in mapping:
+                try:
+                    selection = selection + "(resname " + mapping[atom]['RESIDUE'] + " and name " + mapping[atom]['ATOMNAME'] + ") or "
+                except:
+                                   
+                #for line in f:
+                #    if len(line.split()) > 2 and "Individual atoms" not in line:
+                #        selection = selection + "(resname " + line.split()[2] + " and name " + line.split()[1] + ") or "
+                #    elif "Individual atoms" in line:
+                #        continue
+                #    else:
+                    selection = "resname " + readme['COMPOSITION'][key_mol]['NAME']
                         #print(selection)
-                        break
-        selection = selection.rstrip(' or ')
-        #print("selection    " + selection)
-        molecules = universe.select_atoms(selection)
-        #print("molecules")
-        #print(molecules)
-        if molecules.n_residues > 0:
-            lipids.append(universe.select_atoms(selection))
+                #        break
+            selection = selection.rstrip(' or ')
+            #print("selection    " + selection)
+            molecules = universe.select_atoms(selection)
+          #  print("molecules")
+          #  print(molecules)
+            if molecules.n_residues > 0:
+                lipids.append(universe.select_atoms(selection))
             #print(lipids) 
     # join all the selected the lipids together to make a selection of the entire membrane and calculate the
     # z component of the centre of mass of the membrane
-    membrane = universe.select_atoms("")
+    membrane = universe.select_atoms("")   # causes a warning: UserWarning: Empty string to select atoms, empty group returned, but it's ok
     R_membrane_z = 0
-    if lipids!= []:
+    if lipids != []:
         for i in range(0,len(lipids)):
             membrane = membrane + lipids[i]
             #print("membrane") 
             #print(membrane)  
     R_membrane = membrane.center_of_mass()[2]
+    #print(R_membrane)
     return R_membrane
     
-
 def headgroupAtoms(readme,lipid):
     mapping_file = loadMappingFile(readme['COMPOSITION'][lipid]['MAPPING'])
     #returns atom names labeled as belonging to headgroup in the mapping file
@@ -102,7 +109,8 @@ def headgroupAtoms(readme,lipid):
     return headgroup
     
 
-path = '../../Databank/Data/Simulations/006/559/006559139e730fc43b244726992145c2f37a1461/3c99810c45a83b4ba0e54a69fdea8817498a8930/'
+#path = '../../Databank/Data/Simulations/006/559/006559139e730fc43b244726992145c2f37a1461/3c99810c45a83b4ba0e54a69fdea8817498a8930/'
+path = '../../Databank/Data/Simulations/'
 db_data = databank(path)
 systems = db_data.get_systems()
 
@@ -124,7 +132,17 @@ for system in systems:
         os.system('mkdir ../Data/Flipflops/' + indexingPath.split('/')[0] + '/' + indexingPath.split('/')[1] + '/' + indexingPath.split('/')[2] + '/' + indexingPath.split('/')[3]) 
         
     flipflop_file = DATAdir + 'flipflop.dat' #change later
-                                      
+
+    if (os.path.isfile(flipflop_file)):
+        continue
+
+    #print(system['TYPEOFSYSTEM'])
+    if system['TYPEOFSYSTEM'] == 'miscellaneous':
+        continue
+
+    os.system('cp ' + subdir + '/README.yaml ' + DATAdir + '/')
+   
+
                 
     doi = system['DOI']
     trj=system['TRJ'][0][0]
@@ -135,25 +153,35 @@ for system in systems:
     tpr_url = download_link(doi, tpr)
     EQtime=float(system['TIMELEFTOUT'])*1000
 
-
-    #Download tpr and xtc files to a tmp directory in dir wrk defined in readme file / or should these be put into the same directory as README.yaml ???
-    if (not os.path.isfile(tpr_name)):
-        response = urllib.request.urlretrieve(tpr_url, tpr_name)
+    #if (not os.path.isfile(tpr_name)):
+    #    response = urllib.request.urlretrieve(tpr_url, tpr_name)
                         
-    if (not os.path.isfile(trj_name)):
-        response = urllib.request.urlretrieve(trj_url, trj_name)
+    #if (not os.path.isfile(trj_name)):
+    #    response = urllib.request.urlretrieve(trj_url, trj_name)
                     
-    xtcwhole=subdir + '/whole.xtc'
-    if (not os.path.isfile(xtcwhole)):
-        os.system('echo System | gmx trjconv -f ' + trj_name + ' -s ' + tpr_name + ' -o ' + xtcwhole + ' -pbc mol -b ' + str(EQtime))
-                    
-    u = mda.Universe(tpr_name,xtcwhole)
-                
+    #xtcwhole=subdir + '/whole.xtc'
+    #if (not os.path.isfile(xtcwhole)):
+    #    os.system('echo System | gmx trjconv -f ' + trj_name + ' -s ' + tpr_name + ' -o ' + xtcwhole + ' -pbc mol -b ' + str(EQtime))
+
+    xtccentered = subdir + '/centered.xtc'
+    if (not os.path.isfile(xtccentered)):
+        continue
+
+    try:
+        u = mda.Universe(tpr_name,xtccentered)
+    except:
+        print('Reading of data with MDanalysis failed')
+        continue
+        
     lipids = getLipids(system)
    # print(lipids)            
     end = len(u.trajectory)
                 
     dt = int(u.trajectory.dt)
+
+    #print(end,dt)
+    if dt == 0:
+        continue
     
     start = int(EQtime / dt)
     flipflops = 0
@@ -164,16 +192,14 @@ for system in systems:
     if time_diff < dt:
         skip = 1
     
-    #   print("EQtime " + str(EQtime))
-    #   print("start " + str(start))
-    #   print("dt " + str(dt))
-    #   print("end " + str(end))
-    #   print("skip " + str(skip))
-    #   print("time_diff  " + str(time_diff))
-
+ #   print("EQtime " + str(EQtime))
+ #   print("start " + str(start))
+ #   print("dt " + str(dt))
+ #   print("end " + str(end))
+ #   print("skip " + str(skip))
+ #   print("time_diff  " + str(time_diff))
                 
     frames_lipids = []
-                
 
     for lipid in lipids:
         #save state of the leaflet in previous frames
@@ -186,16 +212,18 @@ for system in systems:
 
         headgroup_atoms_selection = u.select_atoms('resname ' + lipid_resname + ' and name' + headgroup_atoms).split('residue')  
      
-        
+        #print(headgroup_atoms_selection)
      #   print(lipid + "  " +lipid_resname)
         
      #   print(lipid_atoms)
      #   print(lipid_atoms_selection)
      
         #every 500th frame for checking flip flops
-        
-        for ts in u.trajectory[start:end:skip]:
-          #  print("frame " + str(ts.frame))
+
+        frame_index = 0
+
+        for ts in u.trajectory[::skip]:
+            #print("frame " + str(ts.frame), ts)
             time = u.trajectory.time
             
             R_m = membraneCentreOfMass(u, system)
@@ -203,39 +231,56 @@ for system in systems:
             l_leaflet_fr = []
             
             for i, hg in enumerate(headgroup_atoms_selection):   #!!!!
+                #print(i, hg[0].resid)
                 hg_z = hg.center_of_mass()[2]
-                hg_resid = i
+                #hg_resid = i
+                hg_resid = hg[0].resid
                 if hg_z < R_m:
                     l_leaflet_fr.append([hg_resid, 'l1']) #in leaflet 1
                 if hg_z > R_m:
                     l_leaflet_fr.append([hg_resid,'l2']) #in leaflet 2       
 
                                
-           # print("l_leaflet_fr")
-           # print(l_leaflet_fr)
+            #print("l_leaflet_fr")
+            #print(l_leaflet_fr)
             
             #check if lipid has changed leaflet between frames
-            if ts.frame != start and ts.frame != start + skip and ts.frame != start + 2*skip: # first two frames
+            #if ts.frame != start and ts.frame != start + skip and ts.frame != start + 2*skip: # first two frames
              #   print(previous_leaflet)
-                for j in range(0,len(l_leaflet_fr)-1):
+
+            previous_leaflet.append(l_leaflet_fr.copy())
+            
+            if frame_index < 2:     #Skip first two frames
+                frame_index += 1
+                continue
+
+            for j in range(0,len(l_leaflet_fr)-1):
                 
-                    index_1 = int(ts.frame/(skip)-start)-1 
-                    index_2 = int(ts.frame/(skip)-start)-2 
-                    
-                   # print(previous_leaflet[index_1])
-                   # print(previous_leaflet[index_2])
-                    
-                    
-                    # is lipid location in current frame different from the lipid location in the two previous frames  
-                    if l_leaflet_fr[j][0] != previous_leaflet[index_1][j][0] and l_leaflet_fr[j][1] == previous_leaflet[index_1][j][1] and l_leaflet_fr[j][0] != previous_leaflet[index_2][j][0] and l_leaflet_fr[j][1] == previous_leaflet[index_2][j][1]: # RESID SAME BUT LEAFLET IS DIFFERENT
-                        #print("FLIP FLOP")
-                        flipflops += 1
-                        res_index = l_leaflet_fr[j][1]
-                        frames_lipids.append([str(time - time_diff) + ' ' + str(time) + '   ' + lipid + ' ' + str(res_index) + ' ' + str(ts.frame - skip) + ' ' + str(ts.frame)    ])
+                #index_1 = int(ts.frame/(skip)-start)-1 
+                #index_2 = int(ts.frame/(skip)-start)-2 
 
+                index_1 = frame_index-1
+                index_2 = frame_index-2
+                
+                #print(previous_leaflet[index_1])
+                #print(previous_leaflet[index_2])
+                
+                #print(l_leaflet_fr[j][0], l_leaflet_fr[j][1])
+                #print(previous_leaflet[index_1][j][0], previous_leaflet[index_1][j][1])
+                #print(frame_index, start)
+                #print(previous_leaflet[frame_index-1])
+                # is lipid location in current frame different from the lipid location in the two previous frames
+                if l_leaflet_fr[j][0] == previous_leaflet[index_1][j][0] and l_leaflet_fr[j][1] != previous_leaflet[index_1][j][1] and l_leaflet_fr[j][0] == previous_leaflet[index_2][j][0] and l_leaflet_fr[j][1] != previous_leaflet[index_2][j][1]: # RESID SAME BUT LEAFLET IS DIFFERENT
+                    #print("FLIP FLOP")
+                    flipflops += 1
+                    res_index = l_leaflet_fr[j][0]
+                    frames_lipids.append([str(time - time_diff) + ' ' + str(time) + '   ' + lipid + ' ' + str(res_index) + ' ' + str(ts.frame - skip) + ' ' + str(ts.frame)    ])
                                     
+                        
+                                    
+            frame_index += 1
+                
 
-            previous_leaflet.append(l_leaflet_fr.copy())    
                             
        # print(flipflops) 
        
@@ -254,13 +299,14 @@ for system in systems:
         print('number of flip flops in trajectory is ' + str(flipflops) + " in the simulation in directory " + indexingPath)
 
        # save frame nr and lipid name
-        with open(flipflop_file, 'a') as f:
-            for fr in frames_lipids:
-                f.write(str(fr).strip("[]'") + '\n')
-        f.close()
     else:
         print("no flip flops in trajectory")
 
+    with open(flipflop_file, 'a') as f:
+        for fr in frames_lipids:
+            f.write(str(fr).strip("[]'") + '\n')
+    #f.close()
 
+                                    
                 
                 
